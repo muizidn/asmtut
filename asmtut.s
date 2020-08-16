981713f0f8769327c6c29b60447bf08e358145f6
muizidn <muiz.idn@gmail.com>
2020-08-16 16:47:51 +0700

    cmp, bgt, beq

--- asmtut.s
+++ asmtut.s
@@ -9,10 +9,25 @@ Wish me a success
 .global _start
 
 _start:
-	mov r0, #15
-	b other
-	mov r0, #20	
-other:
+	mov r1, #15
+	mov r2, #10
+	cmp r1, r2
+	beq val_equal @ zero flag set
+	bgt r1_gt @ r1 greater, negative flag set
+
+r1_lt:
+	mov r0, #-1
+	b end
+
+r1_gt:
+	mov r0, #1
+	b end
+
+val_equal:
+	mov r0, #0
+	b end
+
+end:
 	mov r7, #1
 	swi 0
 
