777eaac122dfcea816ef74ab15a43f9b247ff911
muizidn <muiz.idn@gmail.com>
2020-08-16 16:36:53 +0700

    add

--- asmtut.s
+++ asmtut.s
@@ -8,24 +8,12 @@ Wish me a success
 
 .global _start
 
-_start: @read
-	mov r7, #3
-	mov r0, #0
-	mov r2, #10
-	ldr r1, =message
-	swi 0
-
-_write:
-	mov r7, #4
-	mov r0, #1
-	mov r2, #5
-	ldr r1, =message
-	swi 0
-
+_start:
+	mov r1, #15
+	mov r2, #0xA @ 10
+	add r0, r1, r2
+	
 end:
 	mov r7, #1
 	swi 0
 
-.data
-message:
-	.ascii " " @ must " " rather than "" ? do research!
