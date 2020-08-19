d428baf22031b945967b9f3f6e4a9aea78c45624
muizidn <muiz.idn@gmail.com>
2020-08-19 07:08:45 +0700

    and

--- asmtut.s
+++ asmtut.s
@@ -1,33 +1,10 @@
-@ Hello this is Asmtut
-/*
-I am learning ARM32 assembly language
-Wish me a success
-*/
-
-.text
-
 .global _start
 
 _start:
-	mov r1, #15
-	mov r2, #10
-	cmp r1, r2
-	beq val_equal @ zero flag set
-	bgt r1_gt @ r1 greater, negative flag set
-
-r1_lt:
-	mov r0, #-1
-	b end
-
-r1_gt:
-	mov r0, #1
-	b end
-
-val_equal:
-	mov r0, #0
-	b end
-
+	mov r1, #5
+	mov r2, #9
+	and r0, r1, r2
+	
 end:
 	mov r7, #1
 	swi 0
-
