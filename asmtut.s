c16ef4a7d8bb879b114c616c985028fa3ac4a53c
muizidn <muiz.idn@gmail.com>
2020-08-21 17:34:30 +0700

    memory storage: str

--- asmtut.s
+++ asmtut.s
@@ -12,10 +12,17 @@ thirty:
 
 _start:
 	ldr r1, addr_fifteen
-	ldr r1, [r1]
+	mov r3, #10
+	str r3, [r1]
 	ldr r2, addr_thirty
-	ldr r2, [r2]
-	add r0, r1, r2
+	mov r3, #30
+	str r3, [r2]
+
+	LDR R1, addr_fifteen @ Load address
+    	LDR R1, [R1] @ Load data using address
+    	LDR R2, addr_thirty @ Load address
+    	LDR R2, [R2] @ Load data using address
+    	ADD R0, R1, R2
 
 end:
 	mov r7, #1
