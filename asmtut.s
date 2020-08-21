73913f8fd7ac9f521b75ad80cce56ce439d0e9c2
muizidn <muiz.idn@gmail.com>
2020-08-21 18:54:00 +0700

    change value in memory

--- asmtut.s
+++ asmtut.s
@@ -1,12 +1,17 @@
+.data
+.balign 4
+info:
+	.word 10
+
+.text
 .global _start
 
 _start:
-	adr r0, info
+	ldr r3, =info
+	mov r4, #100
+	str r4, [r3]
+	ldr r0, [r3]
 
 end:
 	mov r7, #1
 	swi 0
-
-info:
-	.word 10
-
