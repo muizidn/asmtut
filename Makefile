862d254d7c9ffc03cd1d96df6dc530e625c20ad4
muizidn <muiz.idn@gmail.com>
2020-08-21 18:47:12 +0700

    debugging

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ asmtut: asmtut.o
 	ld -o asmtut asmtut.o
 
 asmtut.o: asmtut.s
-	as -o asmtut.o asmtut.s
+	as -g -o asmtut.o asmtut.s
 
 clean:
 	rm *.o asmtut
+
+debug:
+	gdb asmtut
--- asmtut.s
+++ asmtut.s
@@ -1,33 +1,16 @@
-.data
-.balign 4
-fifteen:
-	.word 15
-
-.balign 4
-thirty:
-	.word 30
-
-.text
 .global _start
 
 _start:
-	ldr r1, addr_fifteen
-	mov r3, #10
-	str r3, [r1]
-	ldr r2, addr_thirty
-	mov r3, #30
-	str r3, [r2]
+	mov r0, #50
+	
+decrement:
+	sub r0, r0, #3
 
-	LDR R1, addr_fifteen @ Load address
-    	LDR R1, [R1] @ Load data using address
-    	LDR R2, addr_thirty @ Load address
-    	LDR R2, [R2] @ Load data using address
-    	ADD R0, R1, R2
+
+loop:
+	cmp r0, #5
+	bgt decrement
 
 end:
 	mov r7, #1
 	swi 0
-
-addr_fifteen: .word fifteen
-addr_thirty: .word thirty
-
