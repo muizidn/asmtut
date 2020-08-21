780e9fd083fb566c3824e8d201d97bc3a1916491
muizidn <muiz.idn@gmail.com>
2020-08-21 16:01:47 +0700

    loop

--- asmtut.s
+++ asmtut.s
@@ -1,16 +1,19 @@
 .global _start
 
 _start:
-	mov r1, #9
-	mov r2, #8
-	teq r1, r2
-	beq _bit_set
-	mov r0, #1
-	b end
-
-_bit_set:
 	mov r0, #0
+	mov r1, #1
+	b _continue_loop
+
+_loop:
+	add r0, r0, r1
+
+_continue_loop:
+	cmp r0, #9
+	ble _loop
 
 end:
 	mov r7, #1
 	swi 0
+
+
