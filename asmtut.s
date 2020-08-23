3e7fbf3658faa6b8ffaec3ceba737a85b7d5114a
muizidn <muiz.idn@gmail.com>
2020-08-23 16:49:05 +0700

    block transfer: ldmia

--- asmtut.s
+++ asmtut.s
@@ -1,12 +1,13 @@
-.data
+.align 2
 numbers:
-	.byte 1, 2, 3, 4, 5
+	.word 1, 2, 3, 4, 5
 
 .text
 .global _start
 _start:
-	ldr r3, =numbers
-	ldrb r0, [r3, #4]
+	adr r3, numbers
+	ldmia r3, {r5-r8}
+	mov r0, r6
 
 end:
 	mov r7, #1
