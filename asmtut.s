283c95a859241dcaa52e4f01d4505a7e141f2bb9
muizidn <muiz.idn@gmail.com>
2020-08-30 10:41:34 +0700

    stack pointer

--- asmtut.s
+++ asmtut.s
@@ -1,14 +1,16 @@
-.align 2
-numbers:
-	.word 1, 2, 3, 4, 5
-
-.text
 .global _start
+
 _start:
-	adr r3, numbers
-	ldmia r3, {r5-r8}
-	mov r0, r6
+	mov r1, #1
+	mov r2, #2
+	
+	str r1, [sp, #-4]!
+	str r2, [sp, #-4]!
+
+	ldr r0, [sp], #+4
+	ldr r0, [sp], #+4
 
 end:
 	mov r7, #1
 	swi 0
+
