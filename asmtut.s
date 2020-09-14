24ad3e45d276dbd556aa16f55794e45bea40aaa8
muizidn <muiz.idn@gmail.com>
2020-09-14 12:43:49 +0700

    equ directive

--- asmtut.s
+++ asmtut.s
@@ -1,19 +1,10 @@
 .global _start
-
 _start:
-	mov r4, #1
-	mov r5, #2
-
-	stmdb sp!, {r4, r5}
-	mov r4, #3
-	mov r5, #4
-	add r0, r4, #0
-	add r0, r0, r5
-	
-	ldmia sp!, {r4, r5}
-	add r0, r0, r4
-	add r0, r0, r5
+	mov r0, #two
 
 end:
 	mov r7, #1
 	swi 0
+
+.data
+.equ two, 2
